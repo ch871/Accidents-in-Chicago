6e377a1596860ebd9b7ab1103f64a17a77fed64a
chaim <shterenchaim@gmail.com>
2024-10-10 10:21:44 +0300

    create requirements and connenct to database

---
+++ database/connect.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://172.20.158.142:27017')
+accidents_db = client['accidents_db']
+
+monthly = accidents_db['monthly']
+weekly = accidents_db['weekly']
+daily = accidents_db["daily"]
+
